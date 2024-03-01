47dad7eece4ea981ed60a242f191990bc7726077
Yiqi Chen <113337711+Chen111777@users.noreply.github.com>
2024-03-01 17:28:45 +0800

    Update fig2_epsilon_above_and_below.m

--- fig2_epsilon_above_and_below.m
+++ fig2_epsilon_above_and_below.m
@@ -3,11 +3,13 @@ clear; clc; close all
 paths = {'~/WRFV4.5.1/a50_exp3',...
 '~/WRFV4.5.1/a2000_exp3',...
 '~/WRFV4.5.1/a10000_exp3','~/WRFV4.5.1/a50000_exp3'};
-load lwcad_exp3_a50_2000_10000_50000
-xna=[50,2000,10000,50000]; % [50,100,200,500,1000,2000,5000,10000,20000,50000];
-InterFileName = 'dat_fig2_4cases_exp3';
+aer_tick=[50,2000,10000,50000]; % [50,100,200,500,1000,2000,5000,10000,20000,50000];
+InputFileName = 'lwcad_4cases_exp3.mat';
+InterFileName = 'dat_fig2_4cases_exp3.mat';
+OutFigName = 'fig2_exp3';
 
-n_na = length(paths);
+load(InputFileName)
+len_aer = length(aer_tick);
 global m r varls 
 r = 2.^(1:1/3:35/3); % bin radius
 m = 4/3*pi.*r2.*r2.*r2/10^9; % bin mass
@@ -23,12 +25,12 @@ ph = phb+php; % because ph merely varies with time
 [nx,~,nz]=size(ph);
 nz=nz-1;
 hh = squeeze(mean(mean(ph(:,:,2:end)-ph(:,:,1:end-1)))/g);
-hh=repmat(hh,1,n_na);
+hh=repmat(hh,1,len_aer);
 clear phb php ph
 mnt_range = 72:6:132;
-
+%% get four-region and domain-mean epsilon and r_mean profiles
 ii = 0;
-[dat_eps,dat_rm] =deal(nan(nz,5,n_na));
+[dat_eps,dat_rm] =deal(nan(nz,5,len_aer));
 for path1 = paths
     ii = ii+1
     [dat_eps(:,:,ii),dat_rm(:,:,ii)] ...
@@ -36,13 +38,12 @@ for path1 = paths
 end
 save(InterFileName,'hh','dat_eps','dat_rm')
 %%
+load(InterFileName)
 B = figure('position',[488,358.6,810,303.4]);
 C2=[50,50,50;78.4,124.8,204;140.8,186.4,183.2;224.4,168.96,178.64;204.5,82.6,77.0]/255;
-
-load(InterFileName)
 yh = squeeze(dat_eps(:,1,:));
-[~,n_na]=size(yh);
-for i=1:n_na
+[~,len_aer]=size(yh);
+for i=1:len_aer
      [~,epsmin(i)]=min(yh(1:end*3/5,i));
 end
 for ii=1:5
@@ -51,7 +52,7 @@ for ii=1:5
 end
 X=X';
 Y=Y';
-func_dash_and_solid(repmat(xna',1,10),Y, 1 ,[10,100000],'$N_{a} (cm^{-3})$','log',[0.12,0.46],C2)
+func_dash_and_solid(repmat(aer_tick',1,10),Y, 1 ,[10,100000],'$N_{a} (cm^{-3})$','log',[0.12,0.46],C2)
 set(gca,'xtick',[10,100,1000,10^4,10^5])
 func_dash_and_solid(X,Y, 2 ,[0,30],'$\mathbf{\overline{r} (\mu m)}$','linear',[0.12,0.46],C2)
 
@@ -61,7 +62,7 @@ legend(lgwd,'Location','eastoutside','fontsize',11,'position',[0.775,0.216,0.199
 legend('boxoff')
 text(1.23,0.947636983613718,'AF Ranges','unit','normal',...
     'fontsize',11,'FontName','Times New Roman')
-print('-dpng',B,'fig2_exp3','-r450')
+print('-dpng',B,OutFigName,'-r450')
 %%
 function [dat_eps,dat_rm] =func_regions_eps_rm(path1,mnt_range,ia,nx,nz,lwcad)
 nmnt = length(mnt_range);
@@ -69,14 +70,14 @@ nmnt = length(mnt_range);
 ii = 0;
 for im = mnt_range
     ii = ii+1;
-    pathbin = [cell2mat(path1),'/wrfbin_d01_0001-01-01_0',...
+    ncpath = [cell2mat(path1),'/wrfbin_d01_0001-01-01_0',...
         num2str(floor(im/60),'%01d'),':',num2str(mod(im,60),'%02d'),':00'];
-    [qc,eps,rm,~,~] = func_get_pro(pathbin,nx,nz);
+    [qc,eps,rm,~,~] = func_get_pro(ncpath,nx,nz);
     
     %---------------lwcmax---------------%
 %     af = qc./repmat(max(max(qc,[],1),[],2),nx,nx);
     %---------------lwcad----------------%
-    rho=1/double(ncread(pathbin,'ALT'));% m3/kg
+    rho=1/double(ncread(ncpath,'ALT'));% m3/kg
     lwc = qc.*rho;
     af = lwc./repmat(permute(lwcad(:,(im-66)/6,ia),[3,2,1]),nx,nx);
 
@@ -100,18 +101,18 @@ dat_rm = squeeze(nanmean(rmmean,2));
 dat_eps = squeeze(nanmean(epsmean,2));
 end
 
-function [qc,eps,rm,sig,N] = func_get_pro(pathbin,nx,nz)
+function [qc,eps,rm,sig,N] = func_get_pro(ncpath,nx,nz)
 global m r varls
 nbins = length(varls);
 qc = zeros(nx,nx,nz);
 for ibin=1:nbins
-    q=double(ncread(pathbin,varls(ibin,:)));
+    q=double(ncread(ncpath,varls(ibin,:)));
     qc = qc+q;
 end
 icloud00 = qc>10^-5;
 [N,rm,sig] = deal(zeros(size(icloud00)));
 for ibin=1:nbins
-    q=double(ncread(pathbin,varls(ibin,:)));
+    q=double(ncread(ncpath,varls(ibin,:)));
     q(~icloud00) = nan;
     npdr=q./m(ibin); % 10^6 kg-1
     rm = rm+npdr.*r(ibin);
@@ -119,7 +120,7 @@ for ibin=1:nbins
 end
 rm = rm./N;
 for ibin=1:nbins
-    q=double(ncread(pathbin,varls(ibin,:)));
+    q=double(ncread(ncpath,varls(ibin,:)));
     q(~icloud00) = nan;
     npdr=q./m(ibin);
     sig = sig+npdr.*(rm-r(ibin)).^2;
