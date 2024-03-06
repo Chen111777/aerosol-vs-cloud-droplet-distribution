7ee2a6f67848477d18a6ba7764bd377397ef9348
Yiqi Chen <113337711+Chen111777@users.noreply.github.com>
2024-03-06 12:08:55 +0800

    Update fig3_and_tab1_S_statistics.m

--- fig3_and_tab1_S_statistics.m
+++ fig3_and_tab1_S_statistics.m
@@ -1,13 +1,21 @@
 clear; clc; close all
 % --------- change these ------------%
-paths = {'~/WRFV4.5.1/a50_exp3',...
-'~/WRFV4.5.1/a2000_exp3',...
-'~/WRFV4.5.1/a10000_exp3','~/WRFV4.5.1/a50000_exp3'};
-aer_tick=[50,2000,10000,50000];%[50,100,200,500,1000,2000,5000,10000,20000,50000];
-InputFileName = 'lwcad_4cases_exp3.mat';
-InterFileName = 'dat_fig3_4cases_exp2.mat';
-OutFigName = 'fig3_exp2';
-nz = 180; nx = 100;
+% paths = {'~/WRFV4.5.1/a50_exp2';...
+% '~/WRFV4.5.1/a2000_exp2';...
+% '~/WRFV4.5.1/a10000_exp2';...
+% '~/WRFV4.5.1/a50000_exp2'};
+% aer_tick=[50,2000,10000,50000];
+paths = {'~/WRFV4.5.1/a50';'~/WRFV4.5.1/a100';'~/WRFV4.5.1/a200';'~/WRFV4.5.1/a500';...
+'~/WRFV4.5.1/a1000';'~/WRFV4.5.1/a2000';'~/WRFV4.5.1/a5000';...
+'~/WRFV4.5.1/a10000';'~/WRFV4.5.1/a20000';'~/WRFV4.5.1/a50000'};
+aer_tick= [50,100,200,500,1000,2000,5000,10000,20000,50000];
+InputFileName = 'lwcad_10cases_exp1_75to135.mat';
+InterFileName = 'dat_fig3_10cases_exp1.mat';
+OutFigName = 'fig3_exp1';
+mnt_interval = 6;
+mnt_tick = 78:mnt_interval:132;
+nz = 180;%80 
+nx = 100;%50;
 
 len_aer = length(aer_tick);
 nbins = 33;
@@ -17,17 +25,16 @@ for ibin = 1:nbins
 end
 a=2.53E12; % coefficients for S
 b=5.42E3;
-mnt_range = 72:6:132;
-nmnt = length(mnt_range);
-s_bnd=[-21:0.01:7]; % boundaries of S bins, unit: %
+s_bnd=[0:0.01:7]; % boundaries of S bins (%), exclude negative values
 n_sbin = length(s_bnd)-1;
 %% get S pdf
-ii = 0;
-for accn=aer_tick
-    ii = ii+1
-    nmnt=zeros(1,4);
-    for im = mnt_range
-    ncpath = [cell2mat(paths(ii)),'/wrfbin_d01_0001-01-01_0',...
+load(InputFileName)
+dat = zeros(len_aer,n_sbin,4);
+for ia = 1:len_aer
+    ia
+    n_exist=zeros(1,4);
+    for im = mnt_tick
+    ncpath = [cell2mat(paths(ia)),'/wrfbin_d01_0001-01-01_0',...
           num2str(floor(im/60),'%01d'),':',num2str(mod(im,60),'%02d'),':00'];
     qc = zeros(nx,nx,nz);
     for ibin=1:nbins
@@ -48,7 +55,7 @@ for accn=aer_tick
     %---------------lwcad----------------%
     rho=1/double(ncread(ncpath,'ALT'));% m3/kg
     lwc = qc.*rho;
-    af = lwc./repmat(permute(lwcad(:,(im-66)/6,ii),[3,2,1]),nx,nx);
+    af = lwc./repmat(permute(lwcad(:,(im-mnt_tick(1)+mnt_interval)/mnt_interval,ia),[3,2,1]),nx,nx);
 for iaf=1:4
     if iaf==1 
     ss = s0(qc>10^-5 & af<=0.1);
@@ -62,17 +69,19 @@ for iaf=1:4
     if isempty(ss)
         continue % no this region, no S, so skip the next step.
     else
-    nmnt(iaf)=nmnt(iaf)+1;
+    n_exist(iaf)=n_exist(iaf)+1;
     for ix = 1:n_sbin
         n=length(ss(ss>s_bnd(ix) & ss<s_bnd(ix+1)));
-        dat(ii,ix,iaf) = dat(ii,ix,iaf)+n/length(ss); % distribute S into bins
+        dat(ia,ix,iaf) = dat(ia,ix,iaf)+n/length(ss); 
+        % distribute S into bins; SS in denominator includes negative values
     end
     end
 end
     
     end
 for iaf=1:4
-    dat(ii,:,iaf) = dat(ii,:,iaf)/nmnt(iaf); % ratio of S at a specific bin to all S, averaged over time
+    dat(ia,:,iaf) = dat(ia,:,iaf)/n_exist(iaf); 
+    % ratio of S at a specific bin to all S, averaged over time
 end 
 end
 save(InterFileName,'dat')
@@ -80,28 +89,23 @@ save(InterFileName,'dat')
 load(InterFileName)
 s_tick=(s_bnd(2:end)+s_bnd(1:end-1))/2;
 [sbar,sbar_trun,epss,epss_trun,skew_trun] = deal(ones(len_aer,4));
-for ip = 1:len_aer
-    ip
+for ia = 1:len_aer
     for iaf=1:4
-    pdf=dat(ip,:,iaf);
-    rt(ip,iaf) = sum(pdf(s_tick<0))/sum(pdf)*100;
+    pdf=dat(ia,:,iaf);
+    rt(ia,iaf) = 100-sum(pdf)*100;
     id = 0;
-    pdf(s_tick<0) = []; % exclude negative value
-    s_tick(s_tick<0) = [];
-    s_tick(pdf==0) = [];
-    pdf(pdf==0) = [];
-    sbar(ip,iaf)=sum(s_tick.*pdf)/sum(pdf);
-    std=sqrt(sum((s_tick-sbar(ip,iaf)).^2.*pdf)/sum(pdf));
-    epss(ip,iaf)=std/sbar(ip,iaf);
-    while skew_trun(ip,iaf)>0.05
+    sbar(ia,iaf)=sum(s_tick.*pdf)/sum(pdf);
+    sigs=sqrt(sum((s_tick-sbar(ia,iaf)).^2.*pdf)/sum(pdf));
+    epss(ia,iaf)=sigs/sbar(ia,iaf);
+    while skew_trun(ia,iaf)>0.05
         pdf(end-id) = 0; % truncate from right to left
         sbar1=sum(s_tick.*pdf)/sum(pdf);
-        std=sqrt(sum((s_tick-sbar1).^2.*pdf)/sum(pdf));
-        skew_trun(ip,iaf)=sum(((s_tick-sbar1)/std).^3.*pdf)/sum(pdf);
+        sigs=sqrt(sum((s_tick-sbar1).^2.*pdf)/sum(pdf));
+        skew_trun(ia,iaf)=sum(((s_tick-sbar1)/sigs).^3.*pdf)/sum(pdf);
         id = id+1;
     end
-    epss_trun(ip,iaf)=std/sbar1;
-    sbar_trun(ip,iaf)=sbar1;
+    epss_trun(ia,iaf)=sigs/sbar1;
+    sbar_trun(ia,iaf)=sbar1;
 end
 end
 num2str(rt','%7.3f') % display negative S ratio - Table 1
@@ -111,9 +115,9 @@ num2str(rt','%7.3f') % display negative S ratio - Table 1
 B = figure('position',[488,358.6,810,303.4]);
 C2=[50,50,50;78.4,124.8,204;140.8,186.4,183.2;224.4,168.96,178.64;204.5,82.6,77.0]/255;
 X=repmat(aer_tick',1,4);
-func_dash_and_solid(X,[epss(:,3:4),epss_trun(:,3:4)], 1 ,'$N_{a} (cm^{-3})$','$\epssilon_{S} (S>0)$',C2(end-1:end,:))
+func_dash_and_solid(X,[epss(:,3:4),epss_trun(:,3:4)], 1 ,'$N_{a} (cm^{-3})$','$\epsilon_{S} (S>0)$',C2(end-1:end,:))
 func_dash_and_solid(X,[sbar(:,3:4),sbar_trun(:,3:4)], 2 ,'$N_{a} (cm^{-3})$','$\mathbf{\overline{S}} (\%, S>0)$',C2(end-1:end,:))
-lgwd = {'0.5~0.85, Original','0.5~0.85, Zero-skewed','0.85~1.0, Original','0.85~1.0, Zero-skewed'};
+lgwd = {'0.5~0.85, Original';'0.5~0.85, Zero-skewed';'0.85~1.0, Original';'0.85~1.0, Zero-skewed'};
 legend(lgwd,'Location','eastoutside','fontsize',11,'position',[0.783,0.396,0.200,0.289])
 set(gca,'position',[0.455,0.19,0.31,0.77])
 legend('boxoff')
